90ff4f18a38852bda78e7d9a93d33031553abe4d
Dragon7Night <josesito7valdes@gmail.com>
2025-12-17 15:06:49 -0300

    2do commit

--- reservaApp/forms.py
+++ reservaApp/forms.py
@@ -1,66 +1,17 @@
 from django import forms
-
 from django.core import validators
 from reservaApp.models import Estado, Reserva
 
 
-class EstadoForm(forms.Form):
-    estado = forms.CharField(
-    widget=forms.TextInput(attrs={
-        'class':'form-control'
-    }),
-    label='Estado:')
-
 class EstadoForm(forms.ModelForm):
-
     class Meta:
         model = Estado
         fields = ['estado']
 
     estado = forms.CharField(
-    widget=forms.TextInput(attrs={
-        'class':'form-control'
-    }),
-    label='Estado:')
-
-
-class ReservaForm(forms.Form):
-
-    nombre_persona = forms.CharField(validators=[
-        validators.MinLengthValidator(1), 
-        validators.MaxLengthValidator(60)],
-        widget=forms.TextInput(attrs={"class": "form-control"}),
-        label="Nombre de la persona:"
+        widget=forms.TextInput(attrs={'class': 'form-control'}),
+        label='Estado:'
     )
-
-    telefono = forms.CharField(
-        widget=forms.TextInput(attrs={"class": "form-control"}),
-        label="Teléfono:"
-    )
-
-    fecha = forms.DateField(
-        widget=forms.DateInput(attrs={"class": "form-control", "type": "date"}),
-        label="Fecha:"
-    )
-
-    hora = forms.TimeField(
-        widget=forms.TimeInput(attrs={"class": "form-control", "type": "time"}),
-        label="Hora:"
-    )
-
-    cant_personas = forms.IntegerField(validators=[
-        validators.MinValueValidator(1)
-        ],
-        widget=forms.NumberInput(attrs={"class": "form-control", "min": 1}),
-        label="Cantidad de personas:"
-    )
-
-    observacion = forms.CharField(
-        widget=forms.Textarea(attrs={"class": "form-control", "rows": 3}),
-        label="Observación:"
-    )
-
-    estado = forms.ModelChoiceField(queryset=Estado.objects.all(), label='Seleccione una de las opciones')
 
 
 class ReservaForm(forms.ModelForm):
@@ -68,15 +19,17 @@ class ReservaForm(forms.ModelForm):
         model = Reserva
         fields = '__all__'
 
-    nombre_persona = forms.CharField(validators=[
-        validators.MinLengthValidator(1), 
-        validators.MaxLengthValidator(60)],
+    nombre_persona = forms.CharField(
+        validators=[
+            validators.MinLengthValidator(1),
+            validators.MaxLengthValidator(60)
+        ],
         widget=forms.TextInput(attrs={"class": "form-control"}),
         label="Nombre de la persona:"
     )
 
-    telefono = forms.CharField(
-        widget=forms.TextInput(attrs={"class": "form-control"}),
+    telefono = forms.IntegerField(
+        widget=forms.NumberInput(attrs={"class": "form-control"}),
         label="Teléfono:"
     )
 
@@ -90,9 +43,8 @@ class ReservaForm(forms.ModelForm):
         label="Hora:"
     )
 
-    cant_personas = forms.IntegerField(validators=[
-        validators.MinValueValidator(1)
-        ],
+    cant_personas = forms.IntegerField(
+        validators=[validators.MinValueValidator(1)],
         widget=forms.NumberInput(attrs={"class": "form-control", "min": 1}),
         label="Cantidad de personas:"
     )
@@ -102,7 +54,7 @@ class ReservaForm(forms.ModelForm):
         label="Observación:"
     )
 
-    estado = forms.ModelChoiceField(queryset=Estado.objects.all(), label='Seleccione una de las opciones')
-
-
-
+    estado = forms.ModelChoiceField(
+        queryset=Estado.objects.all(),
+        label='Seleccione una de las opciones'
+    )
