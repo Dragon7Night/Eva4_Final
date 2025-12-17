1671ced42d422bc5d9f22429563de510437219ec
Dragon7Night <josesito7valdes@gmail.com>
2025-12-17 15:39:17 -0300

    jol

--- reservaApp/admin.py
+++ reservaApp/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+
+from reservaApp.models import Estado, Reserva
+
+
+
+# CRUD del administrador para Cargo 
+class EstadoAdmin(admin.ModelAdmin):
+    list_display = ['id','estado']
+
+admin.site.register(Estado, EstadoAdmin)
+
+
+# CRUD del administrador para el Empleado
+class ReservaAdmin(admin.ModelAdmin):
+    list_display = ['id','nombre_persona','telefono','fecha','hora','cant_personas','observacion','estado']
+
+admin.site.register(Reserva, ReservaAdmin)
+
+
+
